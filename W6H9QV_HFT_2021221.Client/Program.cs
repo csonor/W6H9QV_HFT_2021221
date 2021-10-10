10a27b1938641ced0a926ea79b36bbd3b707b23d
Csonor <Csonor@CSONOR>
2021-10-10 16:41:51 +0200

    Add project files.

---
+++ W6H9QV_HFT_2021221.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace W6H9QV_HFT_2021221.Client
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ W6H9QV_HFT_2021221.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace W6H9QV_HFT_2021221.Repository
+{
+	public class Class1
+	{
+	}
+}
