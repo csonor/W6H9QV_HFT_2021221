ed296d9af119590f91b14587da917e3560dfe698
Csongor Dobák <csdobak2@gmail.com>
2021-11-13 15:26:31 +0100

    Removed unnecessary usings

--- W6H9QV_HFT_2021221.Models/Country.cs
+++ W6H9QV_HFT_2021221.Models/Country.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace W6H9QV_HFT_2021221.Models
 {
