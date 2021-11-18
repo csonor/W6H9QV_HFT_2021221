e7129bcf6765d8c756f87c63c3366f477ab8d6ec
Csongor Dobák <csdobak2@gmail.com>
2021-11-18 22:01:46 +0100

    Added round to averages

--- W6H9QV_HFT_2021221.Models/CountryAveragePopulation.cs
+++ W6H9QV_HFT_2021221.Models/CountryAveragePopulation.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace W6H9QV_HFT_2021221.Models
@@ -6,7 +7,7 @@ namespace W6H9QV_HFT_2021221.Models
 	public class CountryAveragePopulation
 	{
 		public string Name { get; set; }
-		public double Avg { get => CountyAveragePopulation.Average(x => x.Avg); }
+		public double Avg { get => Math.Round(CountyAveragePopulation.Average(x => x.Avg), 2); }
 		public IList<CountyAveragePopulation> CountyAveragePopulation { get; set; }
 	}
 
@@ -14,6 +15,7 @@ namespace W6H9QV_HFT_2021221.Models
 	{
 		public string Name { get; set; }
 		public string CountryName { get; set; }
-		public double Avg { get; set; }
+		public double Avg { get => Math.Round(avg, 2); set => avg = value; }
+		double avg;
 	}
 }
