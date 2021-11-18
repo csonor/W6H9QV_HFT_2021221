771c959ac8ad8f06d8e4e282673660ebd49de3cf
Csongor Dobák <csdobak2@gmail.com>
2021-11-18 19:59:35 +0100

    Added AveragePop classes

---
+++ W6H9QV_HFT_2021221.Models/CountryAveragePopulation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace W6H9QV_HFT_2021221.Models
+{
+	public class CountryAveragePopulation
+	{
+		public string Name { get; set; }
+		public double Avg { get => CountyAveragePopulation.Average(x => x.Avg); }
+		public IList<CountyAveragePopulation> CountyAveragePopulation { get; set; }
+	}
+
+	public class CountyAveragePopulation
+	{
+		public string Name { get; set; }
+		public string CountryName { get; set; }
+		public double Avg { get; set; }
+	}
+}
