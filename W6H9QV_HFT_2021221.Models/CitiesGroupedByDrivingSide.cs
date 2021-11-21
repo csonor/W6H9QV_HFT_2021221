846f504aa2eff71ba1ad09f0c994645a9ab69641
Csongor Dobák <csdobak2@gmail.com>
2021-11-21 12:06:23 +0100

    Added CitiesGroupedByDrivingSide helper model

---
+++ W6H9QV_HFT_2021221.Models/CitiesGroupedByDrivingSide.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W6H9QV_HFT_2021221.Models
+{
+	public class CitiesGroupedByDrivingSide
+	{
+		public DrivingSide DrivingSide { get; set; }
+		public List<City> Cities { get; set; }
+		public int SumPopulation { get => Cities.Sum(x => x.Population); }
+	}
+}
