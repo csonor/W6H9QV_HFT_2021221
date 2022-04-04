c7795a20786233abca52a364ffd7c392de6eaa34
Csongor Dobák <csdobak2@gmail.com>
2022-04-04 21:05:43 +0200

    Added create and delete

--- W6H9QV_HFT_2021221.JsClient/wwwroot/script.js
+++ W6H9QV_HFT_2021221.JsClient/wwwroot/script.js
@@ -1,14 +1,20 @@
 ﻿let countries = [];
 
-fetch('http://localhost:7649/country')
-	.then(x => x.json())
-	.then(y => {
-		countries = y;
-		display();
-		console.log(y);
-	});
+getdata();
+
+async function getdata() {
+	await
+		fetch('http://localhost:7649/country')
+			.then(x => x.json())
+			.then(y => {
+				countries = y;
+				display();
+				console.log(y);
+			});
+}
 
 function display() {
+	document.getElementById('results').innerHTML = "";
 	countries.forEach(x => {
 		document.getElementById('results').innerHTML +=
 			"<tr><td>" + x.id + "</td><td>"
@@ -18,14 +24,56 @@ function display() {
 			+ x.population + "</td><td>"
 			+ x.currency + "</td><td>"
 			+ x.drivingSide + "</td><td>"
-			+ `<button type="button"onclick="remove(${x.id})">Delete</button>` + "</td></tr>";
+			+ `<button type="button"onclick="remove(${x.id})">Remove</button>` + "</td></tr>";
 	})
 }
 
 function create() {
-	let name = document.getElementById('countryName')
+	let Cname = document.getElementById('name').value;
+	let CenglishName = document.getElementById('englishName').value;
+	let CcountryCode = document.getElementById('countryCode').value;
+	let Cpopulation = document.getElementById('population').value;
+	let Ccurrency = document.getElementById('currency').value;
+	let CdrivingSide = document.getElementById('drivingSide').value;
+
+	fetch('http://localhost:7649/country', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json', },
+		body: JSON.stringify(
+			{
+				name: Cname,
+				englishName: CenglishName,
+				countryCode: CcountryCode,
+				population: Number(Cpopulation),
+				currency: Ccurrency,
+				drivingSide: Number(CdrivingSide)
+			})
+	})
+		.then(response => response)
+		.then(data => {
+			console.log('Success:', data);
+			getdata();
+		})
+		.catch((error) => { console.error('Error:', error); });
+
+	document.getElementById('name').value = "";
+	document.getElementById('englishName').value = "";
+	document.getElementById('countryCode').value = "";
+	document.getElementById('population').value = "";
+	document.getElementById('currency').value = "";
+	document.getElementById('drivingSide').value = "";
 }
 
 function remove(id) {
-	alert(id);
+	fetch('http://localhost:7649/country/delid/' + id, {
+		method: 'DELETE',
+		headers: { 'Content-Type': 'application/json', },
+		body: null
+	})
+		.then(response => response)
+		.then(data => {
+			console.log('Success:', data);
+			getdata();
+		})
+		.catch((error) => { console.error('Error:', error); });
 }
