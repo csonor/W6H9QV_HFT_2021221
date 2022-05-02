449891bd1095a3eda5f3b34996835b94f8c9e480
Csongor Dobák <csdobak2@gmail.com>
2022-05-02 12:18:58 +0200

    Code cleanup

--- W6H9QV_HFT_2021221.WpfClient/Services/AddOrEditEntityService.cs
+++ W6H9QV_HFT_2021221.WpfClient/Services/AddOrEditEntityService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using W6H9QV_HFT_2021221.Models;
+﻿using W6H9QV_HFT_2021221.Models;
 using W6H9QV_HFT_2021221.WpfClient.Windows;
 
 namespace W6H9QV_HFT_2021221.WpfClient.Services
--- W6H9QV_HFT_2021221.WpfClient/Services/IAddOrEditEntityService.cs
+++ W6H9QV_HFT_2021221.WpfClient/Services/IAddOrEditEntityService.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using W6H9QV_HFT_2021221.Models;
-
-namespace W6H9QV_HFT_2021221.WpfClient.Services
+﻿namespace W6H9QV_HFT_2021221.WpfClient.Services
 {
 	interface IAddOrEditEntityService<T>
 	{
